49beb30f19dfe2d6dcbfd5c911405af9ad68deeb
lucasrequeijo <requeijol@gonzaga.org>
2025-03-08 11:41:27 -0500

    new code

--- hackathonwinners.py
+++ hackathonwinners.py
@@ -1,12 +1,21 @@
 import math
+from geopy.geocoders import Nominatim
 
 coordinates = [(1, 2), (3, 4), (5, 6)]
 
 def calculate_distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-user_x = float(input("Enter your x coordinate: "))
-user_y = float(input("Enter your y coordinate: "))
+def get_device_location():
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode("Your IP Address")  # Replace with actual IP address or use a service to get it
+    return location.longitude, location.latitude
+
+def convert_to_xy(longitude, latitude):
+    return longitude, latitude
+
+user_longitude, user_latitude = get_device_location()
+user_x, user_y = convert_to_xy(user_longitude, user_latitude)
 
 distances = [calculate_distance(user_x, user_y, coord[0], coord[1]) for coord in coordinates]
 
